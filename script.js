0562f456f97459dd544a9bb8cdcc929d698e4121
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-12-21 09:11:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 //complete this code
 class Animal {
 	constructor(species){
-		this._species = species;
+		this.species = species;
 	}
 	get species(){
-		return this._species;
+		return this.species;
 	}
-	makeSound(){
-		console.log("sound");
+	makeSound(sound){
+		console.log(`${sound}`);
 	}
 }
 
 class Dog extends Animal {
 	bark(){
-		console.log("woof");
+		this.makeSound("woof");
 	}
 }
 
 class Cat extends Animal {
 	purr(){
-		console.log("purr");
+		this.makeSound("purr");
 	}
 }
 
